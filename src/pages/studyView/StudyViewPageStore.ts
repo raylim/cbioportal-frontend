0ff51bfd2163e4f65636f0f695b8c813f39575f6
Raymond Lim <raylim@mm.st>
2026-06-25 18:30:09 -0400

    feat(wsi): finish pathology timeline and viewer cleanup

--- src/pages/studyView/StudyViewPageStore.ts
+++ src/pages/studyView/StudyViewPageStore.ts
@@ -356,6 +356,7 @@ import {
     isSurvivalChart,
 } from './charts/survival/StudyViewSurvivalUtils';
 import { allowExpressionCrossStudy } from 'shared/lib/allowExpressionCrossStudy';
+import { shouldHideLegacyHeResourceTab } from 'shared/lib/ResourceUtils';
 import {
     ExtendedClinicalAttribute,
     fetchPatients,
@@ -6734,13 +6735,10 @@ export class StudyViewPageStore
         },
         onResult: defs => {
             if (defs) {
-                const hideLegacyHeTab =
-                    getServerConfig().msk_wsi_tile_server_url !== null &&
-                    getServerConfig().msk_wsi_tile_server_url !== undefined;
                 for (const def of defs)
                     if (
                         def.openByDefault &&
-                        !(hideLegacyHeTab && def.resourceId === 'HE')
+                        !shouldHideLegacyHeResourceTab(def.resourceId)
                     )
                         this.setResourceTabOpen(def.resourceId, true);
             }
